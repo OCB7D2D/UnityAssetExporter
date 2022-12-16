afe05588207bce66dae406d6ad678caa81045054
Marcel Greter <marcel.greter@ocbnet.ch>
2022-12-16 10:07:51 +0100

    Initial working version

---
+++ Exporter Scripts/Runtime/AssetBundleUnity3D.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+namespace UnityAssetExporter
+{
+
+    [CreateAssetMenu(fileName = "AssetBundle", menuName = "Unity3D Bundle", order = 2)]
+    [System.Serializable]
+    public class AssetBundleUnity3D : ScriptableObject
+    {
+
+        [HideInInspector]
+        public string Path = "";
+
+        [HideInInspector]
+        public bool IncludeAssetsFromFolders = false;
+
+        [HideInInspector]
+        public bool SearchFoldersRecursively = false;
+
+        [HideInInspector]
+        public bool CreateDeterministicAssetBundle = true;
+
+        [HideInInspector]
+        public bool AppendHashToAssetBundleName = true;
+
+        [HideInInspector]
+        public int Compression = 1;
+
+        [Space(10)]
+
+        [Tooltip("Drag objects to include in the export here")]
+        public Object[] Objects;
+
+    }
+
+}
+
